515ecb1ed6c990a59d511698277aeef2646597db
Martin Namukombo <mastadict@gmail.com>
2022-01-08 08:59:20 +0100

    Complete change list
    - Modified content body html tag to access links from a non 'Category' wikipedia page.
    - Previous version only collected a single paragraph from each page. This has been modified to multiple paragraphs with a 'page_paragraph_limit' control parameter.
    - min and and max character count has been changed to 50 and 150 respectively.

--- GetPageLinksFromWikiPortals.py
+++ GetPageLinksFromWikiPortals.py
@@ -2,33 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 import os
-import pandas as pd 
+import pandas as pd
 
 from tqdm import tqdm
 import json
 
 
 def get_page_links(driver, portal_link):
-    
     driver.get(portal_link)
-    sections = driver.find_elements_by_class_name("mw-category-group")
+    sections = driver.find_elements_by_class_name("mw-body-content")
     df = pd.DataFrame(columns=["title", "link"])
 
     line = 0
     for section in sections:
-
         section_info = [(elm.text, elm.get_attribute("href")) for elm in section.find_elements_by_tag_name("a")]
-        section_info = pd.DataFrame(section_info, columns = ["title", "link"])
+        section_info = pd.DataFrame(section_info, columns=["title", "link"])
         df = df.append(section_info)
-        
     return df
 
 
-
-#Loading parameters
+# Loading parameters
 with open("parameters.json", "r") as file:
     parameters = json.load(file)
-    
+
 path_to_chome_driver = parameters["path_to_chome_driver"]
 
 data_dir = parameters["data_dir"]
@@ -36,40 +32,35 @@ results_file_name = parameters["results_file_name"]
 visible_browser = parameters["visible_browser"]
 wiki_portal_links = parameters["wiki_portal_links"]
 
-#Loding Wiki portal links
-portal_links_list = pd.read_csv(wiki_portal_links, sep="\n", header=None).iloc[:,0]
+# Loding Wiki portal links
+portal_links_list = pd.read_csv(wiki_portal_links, sep="\n", header=None).iloc[:, 0]
 
-
-#Launching Chrome driver
+# Launching Chrome driver
 options = Options()
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('log-level=3')
 
-if visible_browser!="yes":
-   options.add_argument('--headless')
+if visible_browser != "yes":
+    options.add_argument('--headless')
 
 driver = webdriver.Chrome(path_to_chome_driver, options=options)
 
-
 driver.implicitly_wait(1)
 
+# Link Extraction
+for i, link in enumerate(tqdm(portal_links_list)):
 
-
-#Link Extraction
-for i,link in enumerate(tqdm(portal_links_list)):
-    
-    if i==0:
-        page_links_df  = get_page_links(driver, link)
+    if i == 0:
+        page_links_df = get_page_links(driver, link)
         continue
-        
-    page_links_df = page_links_df.append(get_page_links(driver, link))
-    
 
-#Closing Chrome driver
+    page_links_df = page_links_df.append(get_page_links(driver, link))
+
+# Closing Chrome driver
 driver.close()
 
-#Saving
+# Saving
 
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
